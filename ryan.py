96098c0945377f4ac3c7a16164de897836d40dd3
Jason Fitzpatrick <Jay@envy.bananahammock.local>
2018-07-07 16:39:37 +0100

    Ryan and Kate Pythin

---
+++ ryan.py
@@ -0,0 +1,26 @@
+import turtle
+
+
+turtle.shape("circle")
+turtle.speed(1)
+turtle.color('#ff0000')
+turtle.penup()
+turtle.goto(-60,145)
+turtle.pendown()
+turtle.write('like it daddy')#, None, None, "20pt normal")
+turtle.penup()
+turtle.goto(-90,120)
+turtle.pendown()
+turtle.write('I learned python like a boss' "15pt normal")
+turtle.penup()
+turtle.goto(-30,100)
+turtle.pendown()
+turtle.write('HAHA!!!')#, None, None, "14pt normal")
+turtle.penup()
+turtle.goto(-30,50)
+turtle.pendown()
+
+turtle.write('Testing Testing Testing', move=False, align='left', font=('Arial', 8, 'normal'))
+turtle.penup()
+turtle.goto(-0,00)
+turtle.pendown()
