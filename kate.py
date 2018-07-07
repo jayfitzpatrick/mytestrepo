b05d44d208df930f1a8820b9f398a1743be3b448
Jason Fitzpatrick <Jay@envy.bananahammock.local>
2018-07-07 16:47:46 +0100

    Ryan and Kate Python

---
+++ kate.py
@@ -0,0 +1,23 @@
+#!/bin/python3
+import turtle
+
+
+turtle.color('#ffccff')
+turtle.speed(1)
+turtle.write('daddy this is fun', None, None, "16pt normal")
+turtle.pendown()
+turtle.goto(0,-50)
+turtle.color('#99ffff')
+turtle.speed(10)
+turtle.write('super duper fun', None, None, "16pt normal")
+turtle.pendown()
+turtle.color('#ff99ff')
+turtle.goto(0,-100)
+turtle.write('absalotly', None, None, "16pt normal")
+turtle.pendown()
+turtle.color('#ff0000')
+turtle.speed(10)
+turtle.circle(100)
+turtle.goto(100,-100)
+turtle.forward(100)
+turtle.pendown()
