b890a48a3a49f38fb9e2b4f3de2327750a648d2c
jayfitzpatrick <jayfitzpatrick@gmail.com>
2019-10-14 20:47:42 +0100

    Fixed

--- kate.py
+++ kate.py
@@ -1,19 +1,23 @@
 #!/bin/python3
 import turtle
 
+#needs python3-tools
 
 turtle.color('#ffccff')
 turtle.speed(1)
-turtle.write('daddy this is fun', None, None, "16pt normal")
+#turtle.write('daddy this is fun', None, None, "16pt normal")
+turtle.write('daddy this is fun' "16pt normal")
 turtle.pendown()
 turtle.goto(0,-50)
 turtle.color('#99ffff')
 turtle.speed(10)
-turtle.write('super duper fun', None, None, "16pt normal")
+#turtle.write('super duper fun', None, None, "16pt normal")
+turtle.write('super duper fun' "16pt normal")
 turtle.pendown()
 turtle.color('#ff99ff')
 turtle.goto(0,-100)
-turtle.write('absalotly', None, None, "16pt normal")
+#turtle.write('absalotly', None, None, "16pt normal")
+turtle.write('absalotly' "16pt normal")
 turtle.pendown()
 turtle.color('#ff0000')
 turtle.speed(10)
