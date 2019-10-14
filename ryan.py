061f8b440d0e9a05aa0a231d5c99b2721d66af29
jayfitzpatrick <jayfitzpatrick@gmail.com>
2019-10-14 20:41:51 +0100

    Fixed

--- ryan.py
+++ ryan.py
@@ -1,6 +1,7 @@
 #!/bin/python3
 import turtle
 
+# needs python3-tools
 
 turtle.shape("circle")
 turtle.speed(1)
@@ -24,5 +25,5 @@ turtle.pendown()
 turtle.write('Testing Testing Testing', move=False, align='left', font=('Arial', 8, 'normal'))
 turtle.penup()
 turtle.goto(-0,00)
-#turtle.pendown()
-#turtle.goto(0.0)
+turtle.pendown()
+turtle.goto(0,0)
